ae7d233b27bf6eeee6255f20bd8541a30d121364
Oleksandr Diachenko <dyachenko664@gmail.com>
2025-04-21 11:08:04 +0300

    create dashboard layout

--- money-tracker-web/src/app/dashboard/components/transactions/transactions.component.ts
+++ money-tracker-web/src/app/dashboard/components/transactions/transactions.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TransactionsFilterModalComponent } from '../../../shared/modals/transactions-filter-modal/transactions-filter-modal.component';
 
 @Component({
   selector: 'app-transactions',
-  imports: [],
+  imports: [MatButton],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.scss'
 })
 export class TransactionsComponent {
 
+  readonly dialog = inject(MatDialog);
+
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(TransactionsFilterModalComponent, {
+      // data: {},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+    });
+  }
 }
--- money-tracker-web/src/app/dashboard/dashboard.component.ts
+++ money-tracker-web/src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HistoryComponent } from './components/history/history.component';
 import { TransactionsComponent } from './components/transactions/transactions.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, HistoryComponent, TransactionsComponent],
+  imports: [
+    RouterOutlet,
+    HistoryComponent,
+    TransactionsComponent,
+    MatIconModule,
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
-  providers: []
+  providers: [],
 })
 export class DashboardComponent {
 
